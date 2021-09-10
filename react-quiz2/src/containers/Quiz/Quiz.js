8b9947baeae8f26eac194222ec616266fe9474bc
Gritchin Sergey <gritchinsergey@yandex.ru>
2021-09-10 16:51:04 +0300

    123

--- react-quiz2/src/containers/Quiz/Quiz.js
+++ react-quiz2/src/containers/Quiz/Quiz.js
@@ -1,12 +1,13 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import classes from './Quiz.css';
 
 
 class Quiz extends Component{
     render(){
+        return(
        <div className={classes.Quiz}>
             <h1>Quiz</h1>
-       </div>
+       </div>)
     }
 }
 
