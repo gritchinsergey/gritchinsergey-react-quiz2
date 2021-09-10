22ebe2a324b567e68ea3202f5c5c886570a3fd38
Gritchin Sergey <gritchinsergey@yandex.ru>
2021-09-10 16:14:53 +0300

    123

--- react-quiz2/src/App.js
+++ react-quiz2/src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from "react";
 import Layout from './hoc/Layout/Layout';
-
+import Quiz from './containers/Quiz/Quiz';
 
 
 function App() {
   return (
     <Layout>
-      
+      <Quiz></Quiz>
     </Layout>
   );
 }
